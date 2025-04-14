19daa3ea1f2f6895b762244e3f975e6ef3bdefdc
13tyh <yuck5g@gmail.com>
2025-04-14 17:38:38 +0900

    LPページのフッター作成

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,6 @@
 import CallToAction from '@/components/call-to-action';
 import Features from '@/components/features';
+import Footer from '@/components/footer';
 import Hero from '@/components/hero';
 import MarketingHeader from '@/components/marketing-header';
 
@@ -10,7 +11,7 @@ const MarketingPage = () => {
       <Hero />
       <Features />
       <CallToAction />
-      {/* <Footer /> */}
+      <Footer />
     </main>
   );
 };
---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+import Container from '@/components/container';
+
+const Footer = () => {
+  return (
+    <footer className="bg-neutral-900">
+      <Container className="py-12 text-center">
+        <p className="text-base text-neutral-200">
+          &copy; 2024 TechShare. All rights reserved.
+        </p>
+      </Container>
+    </footer>
+  );
+};
+export default Footer;
